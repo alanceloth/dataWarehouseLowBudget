7313b35443f0f5c03d9f8e96e4893f3563661d35
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-02-02 22:36:11 -0300

    build: backend code to process and upload the csv files

---
+++ src/backend.py
@@ -0,0 +1,49 @@
+import pandas as pd 
+from contract import contract
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv("keys/.env")
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+def process_csv(uploaded_file):
+    try:
+        df = pd.read_csv(uploaded_file)
+        
+        extra_cols = set(df.columns) - set(contract.model_fields.keys())
+        if extra_cols:
+            return False, f"CSV file contains extra columns: {', '.join(extra_cols)}"
+
+        missing_cols = set(contract.model_fields.keys()) - set(df.columns)
+        if missing_cols:
+            return False, f"CSV file is missing columns: {', '.join(missing_cols)}"
+
+        #df = df.rename(columns=contract.model_fields)
+        
+        for index, row in df.iterrows():
+            try:
+                _ = contract(**row.to_dict())
+            except Exception as e:
+                raise ValueError(f"Row {index + 1}: {e}")
+        
+        return df, True, None
+    
+    except ValueError as ve:
+        return df, False, str(ve)
+    except Exception as e:
+        return df, False, f"Unexpected Error: {str(e)}"
+    
+def csv_to_sql(df):
+    try:
+        df.to_sql(name="transactions", con=DATABASE_URL, if_exists="replace", index=False)
+        return True, None
+    except Exception as e:
+        return False, str(e)
